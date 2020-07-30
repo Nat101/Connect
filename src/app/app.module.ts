bf06a7095a8311f7e3b6da8db767fc5499aa291a
natalie carlson <nfcarlson@alaska.edu>
2020-07-30 11:20:50 -0800

    added firebase libraries and imports to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
+
+
 @NgModule({
 	declarations: [
 		AppComponent
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		AngularFireModule.initializeApp(environment.firebaseConfig, 'projectName'),
+		AngularFireAuthModule,
+		AngularFirestoreModule,
+		AngularFireStorageModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
